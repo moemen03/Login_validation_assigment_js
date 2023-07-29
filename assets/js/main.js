33a52eb38788023c51eceac78254ed2d39425b13
moemen03 <moemenatia3@gmail.com>
2023-07-29 20:28:48 +0300

    sec commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,10 @@
 // login var
-var email_login = document.getElementById('email'),
-    password_login = document.getElementById('password'),
+var email_l = document.getElementById('email'),
+    password_l = document.getElementById('password'),
     login_btn = document.getElementById('login');
 
 // sign up var
-var username_sign = document.getElementById('username_s'),
-    email_sign = document.getElementById('email_s'),
-    password_sign = document.getElementById('password_s'),
+var username_sign = document.getElementById('username'),
     sign_btn = document.getElementById('signup');
 
 //home var
@@ -41,7 +39,7 @@ else{
 
 
 // regex
-var user_regx = /^[A-Za-z][A-Za-z0-9_]{4,29}$/;
+var user_regx = /^[A-Za-z][A-Za-z0-9_]{3,29}$/;
 var email_regx = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 var pass_regx = /^[a-zA-Z0-9!@#$%^&*]{6,30}$/;
 
@@ -63,59 +61,81 @@ function signUp(){
 
     var temp = {
         name: username_sign.value,
-        email: email_sign.value,
-        password: password_sign.value,
+        email: email_l.value,
+        password: password_l.value,
     }
-    
-    if (arr.length == 0) {
-        arr.push(temp)
-        localStorage.setItem('users', JSON.stringify(arr))
-        box_sign.classList.remove("d-none");
-        return true;
-    }
-    if (isEmailExist()) {
-        box_sign2.classList.remove("d-none");
-    } else {
-        arr.push(temp)
-        localStorage.setItem('users', JSON.stringify(arr))
-        box_sign.classList.remove("d-none");
+    if(username_sign.classList.contains("isvalid") && email_l.classList.contains("isvalid") && password_l.classList.contains("isvalid")){
+        if (arr.length == 0) {
+            arr.push(temp)
+            localStorage.setItem('users', JSON.stringify(arr))
+            box_sign.classList.remove("d-none");
+            return true;
+        }
+        if (isEmailExist()) {
+            box_sign2.classList.remove("d-none");
+        } else {
+            arr.push(temp)
+            localStorage.setItem('users', JSON.stringify(arr))
+            box_sign.classList.remove("d-none");
+        }
     }
 
 }
+
 
 
 
 
 function loginn(){
-
+    if(email_l.value=="" || password_l.value==""){
+        reqInputs.classList.remove("d-none");
+    }
+    else{
+        var pass = password_l.value;
+        var em = email_l.value;
+    
+        if(isEmail_passExist(em, pass)){
+            localStorage.setItem('loginsession', arr[isEmail_passExist(em, pass)-1].name);
+            location.replace(location.hostname +"Login_validation_assigment_js/home.html");
+        }
+        else{
+            reqInputs.classList.remove("d-none");
+            reqInputs.innerHTML="<h6>incorrect Email or password</h6>";
+        }
+    }
 }
-login_btn.addEventListener("click",loginn);
+
+
+
+function logout() {
+    localStorage.removeItem('loginsession');
+    location.replace(location.hostname +"Login_validation_assigment_js/index.html");
+}
+
+// console.log(location.hostname.replace("signUp.html"));
+
+// window.location = 'http://127.0.0.1:5500/';
+
 
 
 
 function isEmailExist(){
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i].email == email_sign.value) {
-            return true;
+        if (arr[i].email == email_l.value) {
+            return (i+1);
         }
     }
 }
-function isEmail_passExist(){
+function isEmail_passExist(em, pass){
     for (var i = 0; i < arr.length; i++) {
-        if ((arr[i].email == email_login.value) &&(arr[i].password == email_login.value) ) {
-            return i+1;
+        if ((arr[i].email == em) &&(arr[i].password == pass) ) {
+            return true;
         }
     }
 }
 
 function is_empty(){
-    if(username_sign.value=="" || email_sign.value=="" || password_sign.value==""){
-        return true;
-    }
-    return false;
-}
-function is_empty_l(){
-    if(email_login.value=="" || password_login.value==""){
+    if(username_sign.value=="" || email_l.value=="" || password_l.value==""){
         return true;
     }
     return false;
@@ -139,30 +159,17 @@ function validate(input,regex,m){
         input.classList.remove("isinvalid");
         input.classList.remove("isvalid");
     }
-    if(username_sign.classList.contains("isvalid") && email_sign.classList.contains("isvalid") && password_sign.classList.contains("isvalid")){
-        reqInputs.classList.add("d-none");
+    if(location.pathname.includes("signup.html")){
+        if(username_sign.classList.contains("isvalid") && email_l.classList.contains("isvalid") && password_l.classList.contains("isvalid")){
+            reqInputs.classList.add("d-none");
+        }
+    }
+    else{
+        if(email_l.classList.contains("isvalid") && password_l.classList.contains("isvalid")){
+            reqInputs.classList.add("d-none");
+        }
     }
 }
-
-// to change color of input box when matches the regex
-
-username_sign.addEventListener("input", function(){
-    validate(username_sign, user_regx,invalidUser);
-});
-
-email_sign.addEventListener("input", function(){
-    validate(email_sign, email_regx,invalidEmail);
-})
-password_sign.addEventListener("input", function(){
-    validate(password_sign, pass_regx,invalidpassword);
-})
-
-
-
-
-
-
-
 
 
 
@@ -177,12 +184,6 @@ function hideModal() {
     box_sign.classList.add("d-none");
     box_sign2.classList.add("d-none");
 }
-
-
-// close when press on X btn
-exitBtn.addEventListener("click", hideModal);
-exitBtn2.addEventListener("click", hideModal);
-
 
 // close Modal when press exit key
 document.addEventListener("keydown", function(ex){
